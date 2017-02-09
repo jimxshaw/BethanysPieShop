71b35be3fd05725ec0b380bb71cc45337e2029b9
Jim Shaw <jimxshaw@gmail.com>
2017-02-08 21:08:11 -0600

    Added constructor to Startup. Registered our db context class.

--- src/BethanysPieShop/Startup.cs
+++ src/BethanysPieShop/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -8,10 +10,25 @@ namespace BethanysPieShop
 {
     public class Startup
     {
+        private IConfigurationRoot _configurationRoot;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                                        .SetBasePath(hostingEnvironment.ContentRootPath)
+                                        .AddJsonFile("appsettings.json")
+                                        .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(
+                    // Connection strings can be found in appsettings.json.
+                    options => options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection"))
+                );
+
             // What AddTransient means is that whenever we invoke an ICategoryRepository we'll get
             // back a new MockCategoryRepository.
             services.AddTransient<ICategoryRepository, MockCategoryRepository>();
