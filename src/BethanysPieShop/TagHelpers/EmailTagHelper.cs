1554f25a89c349e724cb551468f2a5b7c45b545d
Jim Shaw <jimxshaw@gmail.com>
2017-02-12 11:41:29 -0600

    Added a custom email tag helper class. Registered it in _ViewImports and then used the tag helper in Contact Index view.

---
+++ src/BethanysPieShop/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BethanysPieShop.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Address { get; set; }
+        public string Content { get; set; }
+
+        // Th Process method will be invoked when our tag helper is displayed.
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // We want to generate an html anchor tag and then set some attributes and
+            // the content.
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Address);
+            output.Content.SetContent(Content);
+        }
+    }
+}
