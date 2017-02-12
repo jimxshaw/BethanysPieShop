66c25e11eea6b2d033612f95b916a2e86d76ca38
Jim Shaw <jimxshaw@gmail.com>
2017-02-11 21:24:41 -0600

    Add and implemented ShoppingCart class. Added required packages in project.json.

---
+++ src/BethanysPieShop/Models/ShoppingCart.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _appDbContext;
+        private ShoppingCart(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public string ShoppingCartId { get; set; }
+
+        // These are the item the user currently has selected.
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            // Our shopping cart will be implemented using the session service.
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+                .HttpContext.Session;
+
+            // Capture our db context.
+            var context = services.GetService<AppDbContext>();
+            // Checks if the current session already has a cart id string in it.
+            // If not, we create a new guid that serves as the new cart id.
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            // Store the new shopping cart in the session and then return a new 
+            // shopping cart with its new cart id.
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddToCart(Pie pie, int amount)
+        {
+            var shoppingCartItem =
+                    _appDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Pie = pie,
+                    Amount = 1
+                };
+
+                _appDbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _appDbContext.SaveChanges();
+        }
+
+        public int RemoveFromCart(Pie pie)
+        {
+            var shoppingCartItem =
+                    _appDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            var localAmount = 0;
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                    localAmount = shoppingCartItem.Amount;
+                }
+                else
+                {
+                    _appDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+
+            _appDbContext.SaveChanges();
+
+            return localAmount;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ??
+                   (ShoppingCartItems =
+                       _appDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                           .Include(s => s.Pie)
+                           .ToList());
+        }
+
+        public void ClearCart()
+        {
+            var cartItems = _appDbContext
+                .ShoppingCartItems
+                .Where(cart => cart.ShoppingCartId == ShoppingCartId);
+
+            _appDbContext.ShoppingCartItems.RemoveRange(cartItems);
+
+            _appDbContext.SaveChanges();
+        }
+
+
+
+        public decimal GetShoppingCartTotal()
+        {
+            var total = _appDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                .Select(c => c.Pie.Price * c.Amount).Sum();
+            return total;
+        }
+    }
+}
