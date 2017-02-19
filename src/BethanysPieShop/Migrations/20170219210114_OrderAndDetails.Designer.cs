acd021e87cd2a6cdd960d08c5c68eb57df59ccfd
Jim Shaw <jimxshaw@gmail.com>
2017-02-19 15:03:07 -0600

    Added DbSet with Order & Order Detail. Added a migration and updated the database.

--- src/BethanysPieShop/Migrations/20170219210114_OrderAndDetails.Designer.cs
+++ src/BethanysPieShop/Migrations/20170219210114_OrderAndDetails.Designer.cs
@@ -8,9 +8,10 @@ using BethanysPieShop.Models;
 namespace BethanysPieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170219210114_Order and Details")]
+    partial class OrderandDetails
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
