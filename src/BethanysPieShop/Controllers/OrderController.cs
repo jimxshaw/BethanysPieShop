94a9ddabffaaa2e61ba6d6d0ec901bdc21012bb9
Jim Shaw <jimxshaw@gmail.com>
2017-02-19 15:56:29 -0600

    Finished implementing OrderController. Added CheckoutComplete view.

--- src/BethanysPieShop/Controllers/OrderController.cs
+++ src/BethanysPieShop/Controllers/OrderController.cs
@@ -20,8 +20,38 @@ namespace BethanysPieShop.Controllers
             _shoppingCart = shoppingCart;
         }
 
+        [HttpGet]
         public IActionResult Checkout()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+
+                return RedirectToAction("CheckoutComplete");
+            }
+
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thank you for your order. You'll be enjoying your delicious pies soon!";
+
             return View();
         }
     }
