e8c5c81b67262a82fc17f0c7115b2392535896e0
Jim Shaw <jimxshaw@gmail.com>
2017-02-25 16:11:57 -0600

    Added LoginViewModel and AccountController.

---
+++ src/BethanysPieShop/Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            // If the model is invalid then we return to the same view.
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+
+            // Does the user store already have a user by that name? If yes, retrieve that user.
+            var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+
+            // If the user is found then try to sign in with the sign in manager.
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    // If sign in is successful then we return the user to the return url, if that exists, or the
+                    // index home page otherwise.
+                    if (string.IsNullOrEmpty(loginViewModel.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    return Redirect(loginViewModel.ReturnUrl);
+                }
+            }
+
+            ModelState.AddModelError("", "Username/password not found");
+
+            return View(loginViewModel);
+        }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser() { UserName = loginViewModel.UserName };
+                var result = await _userManager.CreateAsync(user, loginViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
